034a0d43fea359cc405d020d0c463eb2f75889ab
Роман Федченко <kuznecovromanv@yandex.ru>
2022-07-26 09:38:31 +0300

    тест

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -9,7 +9,7 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         model = Post
@@ -17,7 +17,7 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         model = Comment
